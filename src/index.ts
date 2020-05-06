243e90ded2e402ef9059a2f5322973bdc3fd2dd7
Angel Torres <avto9711@gmail.com>
2020-05-05 22:50:24 -0400

    basic configuration with express, nodemon and tsc

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("dimelo")
+import express from "express";
+
+const app = express();
+
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT!
+
+app.listen(port, () => {
+    console.log("Runnings on %s and ", port)
+})
